6fe1bf80eec81ad45d406f340e47f69a09d1c86e
Hunseo Son <2423aa@naver.com>
2025-10-05 15:28:03 +0900

    [feat] 전체 SEO 최적화

--- src/theme/DocItem/Layout/index.js
+++ src/theme/DocItem/Layout/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import Layout from '@theme-original/DocItem/Layout';
 import GiscusComponent from '@site/src/components/GiscusComponent';
+import Head from '@docusaurus/Head';
 
 export default function LayoutWrapper(props) {
   return (
     <>
       <Layout {...props} />
       <GiscusComponent />
+    </>
+  );
+}
+
+// for SEO
+export default function LayoutWrapper(props) {
+  const {content} = props;
+  const {metadata} = content;
+  
+  const schema = {
+    '@context': 'https://schema.org',
+    '@type': 'TechArticle',
+    headline: metadata.title,
+    description: metadata.description,
+    datePublished: metadata.editUrl?.lastUpdated,
+  };
+
+  return (
+    <>
+      <Head>
+        <script type="application/ld+json">
+          {JSON.stringify(schema)}
+        </script>
+      </Head>
+      <Layout {...props} />
     </>
   );
 }
