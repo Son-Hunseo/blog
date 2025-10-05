09c920e8a3882b1e01e4d6fbb30b192885ede251
Hunseo Son <2423aa@naver.com>
2025-10-06 00:13:57 +0900

    [chore] 본문 <-> 댓글창 간격 넓히기

--- src/components/GiscusComponent.js
+++ src/components/GiscusComponent.js
@@ -6,19 +6,21 @@ export default function GiscusComponent() {
   const { colorMode } = useColorMode();
 
   return (
-    <Giscus
-      repo="Son-Hunseo/blog"
-      repoId="R_kgDOP2AlTg"
-      category="Q&A"
-      categoryId="DIC_kwDOP2AlTs4CwQsW"
-      mapping="pathname"
-      strict="0"
-      reactionsEnabled="0" // 반응 비활성화
-      emitMetadata="0"
-      inputPosition="bottom"
-      theme={colorMode}
-      lang="ko"
-      loading="lazy"
-    />
+    <div style={{ marginTop: '5rem' }}>
+      <Giscus
+        repo="Son-Hunseo/blog"
+        repoId="R_kgDOP2AlTg"
+        category="Q&A"
+        categoryId="DIC_kwDOP2AlTs4CwQsW"
+        mapping="pathname"
+        strict="0"
+        reactionsEnabled="0" // 반응 비활성화
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme={colorMode}
+        lang="ko"
+        loading="lazy"
+        />
+    </div>  
   );
 }
