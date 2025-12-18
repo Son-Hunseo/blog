f896198f526f3e1052df22d775a717f784055e7e
son-hunseo <thsgnstj0512@gmail.com>
2025-12-18 18:31:27 +0900

    [feat] Algolia 검색 기능 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,11 +104,11 @@ const config = {
       ],
 
       // 검색 기능 algolia - github 소셜 로그인으로 가입함
-      // algolia: {
-      //   appId: '0PCZBRABRY',
-      //   apiKey: 'b533aa69eb09acc01a24f7277fb5e05f',
-      //   indexName: 'blog crawler',
-      // },
+      algolia: {
+        appId: 'KIQYIOJ2YW',
+        apiKey: 'e2d2f3d71fe1ee88a72d8798417c0a02',
+        indexName: 'blog_sonhs_com_kiqyioj2yw_pages',
+      },
 
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
