11a9917ee41f32e73b400cbb2d86834d88530169
Hunseo Son <2423aa@naver.com>
2025-12-19 12:39:34 +0900

    [refactor] 커스텀 리스트 컴포넌트 코드에 코드 설명 주석 추가

--- src/components/SimpleDocList.js
+++ src/components/SimpleDocList.js
@@ -1,3 +1,16 @@
+/*
+해당 카테고리의 index.mdx에 추가할 수 있는 커스텀 컴포넌트 코드
+이 컴포넌트를 카테고리의 index.mdx에 추가하면, 아래 예시처럼 글들이 나옴
+
+기본으로 도큐사우루스에 내장되어있는 카드리스트 컴포넌트가 글이 많아지면 너무 번잡스러워져서 만듬
+
+예시:
+[카테고리]
+- 글1
+- 글2
+- 글3
+...
+*/
 import React from 'react';
 import {useCurrentSidebarCategory} from '@docusaurus/theme-common';
 import Link from '@docusaurus/Link';
