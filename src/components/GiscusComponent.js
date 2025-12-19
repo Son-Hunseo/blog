572ba095a581d9711f57993553dbfa1e6442482a
Hunseo Son <2423aa@naver.com>
2025-12-19 12:42:03 +0900

    [refactor] 댓글(Giscus) 컴포넌트에 주석 추가

--- src/components/GiscusComponent.js
+++ src/components/GiscusComponent.js
@@ -1,3 +1,8 @@
+/*
+Giscus라는 깃헙 디스커스를 댓글로 이용하는 오픈소스를 사용하여
+글 하단에 댓글을 추가하는 컴포넌트
+*/
+
 import React from 'react';
 import Giscus from '@giscus/react';
 import { useColorMode } from '@docusaurus/theme-common';
